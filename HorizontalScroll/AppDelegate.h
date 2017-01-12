e5ecc585ede1e771124cd56f23a400b4521f6062
Anton Orzes <antonorzes@Antons-Mac-mini.local>
2017-01-12 19:34:53 +0100

    Initial Commit

---
+++ HorizontalScroll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HorizontalScroll
+//
+//  Created by Anton Orzes on 12/01/2017.
+//  Copyright © 2017 Anton Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HorizontalScroll/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HorizontalScroll
+//
+//  Created by Anton Orzes on 12/01/2017.
+//  Copyright © 2017 Anton Orzes. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
